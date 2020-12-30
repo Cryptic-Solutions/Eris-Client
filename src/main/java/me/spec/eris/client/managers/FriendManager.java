1aff8c995b2e3cf8cae2a119dbb1cc3a52789b20
femboy-simulator <76745977+femboy-simulator@users.noreply.github.com>
2020-12-30 17:11:43 +0100

    cleaned up friend system

--- src/main/java/me/spec/eris/client/managers/FriendManager.java
+++ src/main/java/me/spec/eris/client/managers/FriendManager.java
@@ -1,7 +1,9 @@
 package me.spec.eris.client.managers;
 
+import me.spec.eris.Eris;
 import me.spec.eris.api.friend.Friend;
 import me.spec.eris.api.manager.Manager;
+import net.minecraft.entity.EntityLivingBase;
 
 public class FriendManager extends Manager<Friend> {
 
@@ -18,5 +20,14 @@ public class FriendManager extends Manager<Friend> {
             }
         }
         return null;
+    }
+
+    public boolean isFriend(EntityLivingBase elb) {
+        for(Friend friend : Eris.getInstance().friendManager.getManagerArraylist()) {
+            if(friend.getFriendName().equalsIgnoreCase(elb.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
