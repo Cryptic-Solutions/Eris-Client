4498cc70d741df385837e1571b09d87387e72bcd
Gardening_Tool <54986171+GardeningTool@users.noreply.github.com>
2021-01-24 15:29:46 -0600

    Cleaned up code, added NoFall Edit
    Also removed the "uwu" part from "Designed with love"

--- src/main/java/net/minecraft/network/play/client/C03PacketPlayer.java
+++ src/main/java/net/minecraft/network/play/client/C03PacketPlayer.java
@@ -7,12 +7,13 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.INetHandlerPlayServer;
 
 public class C03PacketPlayer implements Packet<INetHandlerPlayServer> {
+
     protected double x;
     protected double y;
     protected double z;
     protected float yaw;
     protected float pitch;
-    protected boolean onGround;
+    public boolean onGround;
     protected boolean moving;
     protected boolean rotating;
 
